e8c997293d7190660bc3fde6bb85c541337a0ca7
NimbusScale <joe@nimbusscale.com>
2018-05-25 16:30:51 -0700

    working env deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-export ENV_NAME=$1
-aws cloudformation deploy --template-file stack.yaml --stack-name "db-${ENV_NAME}" --capabilities CAPABILITY_IAM --parameter-overrides EnvName="${ENV_NAME}" DbAdminPassword="Password123!"
+ENV_NAME=$1
+OAUTH_TOKEN=$2
+
+if [ -z ${OAUTH_TOKEN} ]; then
+    aws cloudformation deploy --template-file stack.yaml --stack-name "ebtest-${ENV_NAME}" --capabilities CAPABILITY_IAM --parameter-overrides EnvName="${ENV_NAME}" DbAdminPassword="Password123!"
+else
+    aws cloudformation deploy --template-file stack.yaml --stack-name "ebtest-${ENV_NAME}" --capabilities CAPABILITY_IAM --parameter-overrides EnvName="${ENV_NAME}" DbAdminPassword="Password123!" OauthToken=${OAUTH_TOKEN}
+fi
