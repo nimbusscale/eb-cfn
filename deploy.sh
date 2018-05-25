ea3cff088326e8f9a2bc887d83b280600fab3a32
NimbusScale <joe@nimbusscale.com>
2018-05-25 11:14:02 -0700

    basic env deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export ENV_NAME=$1
-aws cloudformation deploy --template-file stack.yaml --stack-name "db-${ENV_NAME}" --parameter-overrides EnvName="${ENV_NAME}" DbAdminPassword="Password123!"
+aws cloudformation deploy --template-file stack.yaml --stack-name "db-${ENV_NAME}" --capabilities CAPABILITY_IAM --parameter-overrides EnvName="${ENV_NAME}" DbAdminPassword="Password123!"
